6b08a06ec68cf920315b649de1f8098564c41800
Jordan Borja <www.jrdnbrj@gmail.com>
2021-06-18 03:05:49 -0500

    to_base64 function and query getProducts return first image as base64

--- src/services/product_service.py
+++ src/services/product_service.py
@@ -9,19 +9,23 @@ def create_product(**kwargs):
     return Product(**kwargs).save()
 
 def get_products():
-    return Product.objects()
+    products = Product.objects()
+
+    for product in products:
+        product.images[0] = to_base64(product.images[0])
+    
+    return products
 
 def get_product_by_path(path):
     product = Product.objects(path=path).first()
-    
-    images = []
-    for img in product.images:
-        try:
-            image_name = config('URL') + "static/" + img
-            image = base64.b64encode(requests.get(image_name).content)
-            images.append(image.decode('utf-8'))
-        except: pass
-
-    product.images = images
+    product.images = [to_base64(img) for img in product.images]
 
     return product
+
+def to_base64(img_name):
+    try:
+        image_url = config('URL') + "static/" + img_name
+        image = base64.b64encode(requests.get(image_url).content)
+        return image.decode('utf-8')
+    except:
+        return ''
