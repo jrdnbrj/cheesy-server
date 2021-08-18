48377711b2b9a0fd694aeb9beae175cf0cf02735
Jordan Borja <www.jrdnbrj@gmail.com>
2021-08-17 20:07:26 -0500

    Query of datetime

--- src/resolvers/settings/query.py
+++ src/resolvers/settings/query.py
@@ -1,12 +1,17 @@
-from graphene import ObjectType
-from graphene import Field
+from graphene import ObjectType, Field, String
 
 from ...services.settings_service import get_settings
 from ..type import SettingsType
 
+from datetime import datetime
+
 
 class Query(ObjectType):
     get_settings = Field(SettingsType)
+    get_datetime = Field(String)
 
     def resolve_get_settings(parent, info):
         return get_settings()
+
+    def resolve_get_datetime(parent, info):
+        return datetime.utcnow()
